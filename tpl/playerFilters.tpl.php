b3bef64bbfa5201b923e0a9e25b72389814ad240
atm-benoit <benoit.costechareyre@atm-consulting.fr>
2020-12-21 17:33:29 +0100

    fix filter

--- tpl/playerFilters.tpl.php
+++ tpl/playerFilters.tpl.php
@@ -4,9 +4,9 @@ global $db, $player_all, $winner_all, $looser_all;
 dol_include_once('core/class/html.form.class.php');
 
 $formUser = new Form($db);
-$listAllPlayer = $formUser->select_dolusers($player_all, 'player_all', 1);
-$listAllWinner = $formUser->select_dolusers($winner_all, 'winner_all', 1);
-$listAllLooser = $formUser->select_dolusers($looser_all, 'looser_all', 1);
+$listAllPlayer = $formUser->select_dolusers($player_all, 'player_all', 1,0,'','','','0',0,0,'',0,'','',1);
+$listAllWinner = $formUser->select_dolusers($winner_all, 'winner_all', 1, 0, '', '', '', '0', 0, 0, '', 0, '', '', 1);
+$listAllLooser = $formUser->select_dolusers($looser_all, 'looser_all', 1, 0, '', '', '', '0', 0, 0, '', 0, '', '', 1);
 
 print '<div id="filter_match"><table><tr class="liste_titre"><td colspan="100%">';
 print '<label for="player_all">Joueur</label>';
